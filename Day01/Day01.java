c8cdb135a75223fd0cf3deb6563e12cf029c86bb
Tfarley1414 <112511648+Tfarley1414@users.noreply.github.com>
2022-12-01 14:27:52 -0600

    day 01 part 2 WIP
    not finished

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -3,7 +3,7 @@ import java.io.File;
 
 public class Day01 {
 
-    private final static String DATA_FILE = "input.txt";
+    private final static String DATA_FILE = "SampleInput.txt";
 
     public static void part1() {
         Scanner in;
@@ -33,11 +33,36 @@ public class Day01 {
     }
 
     public static void part2() {
+        Scanner in;
+        try {
+            in = new Scanner(new File(DATA_FILE));
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+
+        
+        int mostCal = 0;
+        while (in.hasNext()) {
+            String line = in.nextLine();
+            int cal = 0;
+
+            // get total calories not seperated by space
+            while (!line.equals("") && in.hasNext()) {
+                cal += Integer.parseInt(line);
+                line = in.nextLine();
+            }
+            if (cal > mostCal) {
+                mostCal = cal;
+
+            }
+        }
+        System.out.println(mostCal);
 
     }
 
     public static void main(String[] args) {
-        part1();
+        part2();
     }
 
 }
