b092f2625da01cc3c12775227c93094aab0f2598
Tfarley1414 <112511648+Tfarley1414@users.noreply.github.com>
2022-12-05 19:47:38 -0600

    Day03 part2 complete

--- Day03/Day03.java
+++ Day03/Day03.java
@@ -34,13 +34,34 @@ public class Day03{
     }
 
     public static void part2() throws IOException {
+        int total = 0;
+        Scanner in = new Scanner(new File("input.txt"));
+        while(in.hasNext()){
+            String lineOne = in.nextLine();
+            //String nextLines = in.nextLine() + in.nextLine();
+            String lineTwo = in.nextLine();
+            String lineThree = in.nextLine();
+            //int half = line.length()/2;
+            //String firstHalf = line.substring(0, half);
+            //String secondHalf = line.substring(half);
+
+            for(int i = 0; i < lineOne.length(); i++){
+                String c = lineOne.substring(i, i+1);
+                if(lineTwo.indexOf(c) != -1 && lineThree.indexOf(c) != -1) {
+                    total += decode(c);
+                    break;
+                }
+
+            }
+        }
+        System.out.println(total);
 
     
     }
 
     public static void main(String[] args) {
         try {
-            part1();
+            part2();
         } catch (Exception e){
             System.out.println(e);
         }
