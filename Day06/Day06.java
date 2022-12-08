acbe149cbdd3dd1f1bbd1c3e333d12a367fcf379
Tfarley1414 <112511648+Tfarley1414@users.noreply.github.com>
2022-12-08 13:57:13 -0600

    Day06 part1 complete

--- Day06/Day06.java
+++ Day06/Day06.java
@@ -7,17 +7,56 @@ public class Day06{
 
 
     public static void part1() throws IOException {
-        Scanner in = new Scanner( new File("sampleInput.txt"));
+        Scanner in = new Scanner( new File("input.txt"));
+
+        String input = in.next();
+
+        outerLoop:
+        for(int i =0; i < input.length()-3; i++){
+            String checkingFour = input.substring(i,i+4);
+            char[] chars = checkingFour.toCharArray();
+            for(int p = 0; p<chars.length; p++){
+                for(int q = p+1; q < chars.length; q++){
+                    if(chars[p]==chars[q]) {
+                        continue outerLoop;
+                    }
+                }
+            }
+            System.out.println(i+4);
+            break;
+        }
+
+       
+
+
     }
 
     public static void part2() throws IOException {
+        Scanner in = new Scanner( new File("sampleInput.txt"));
+
+        String input = in.next();
+
+        outerLoop:
+        for(int i =0; i < input.length()-3; i++){
+            String checkingFour = input.substring(i,i+4);
+            char[] chars = checkingFour.toCharArray();
+            for(int p = 0; p<chars.length; p++){
+                for(int q = p+1; q < chars.length; q++){
+                    if(chars[p]==chars[q]) {
+                        continue outerLoop;
+                    }
+                }
+            }
+            System.out.println(i+4);
+            break;
+        }
 
     
     }
 
     public static void main(String...args){
         try{
-            part1();
+            part2();
         } catch (Exception e) {
             System.out.println(e);
         }
