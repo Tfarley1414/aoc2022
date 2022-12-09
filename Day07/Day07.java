70029efb6d492d4bd0ad1d4dbf1315c9b68b9a6b
Tfarley1414 <112511648+Tfarley1414@users.noreply.github.com>
2022-12-09 13:33:33 -0600

    Day07 initial

---
+++ Day07/Day07.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+
+public class Day07{
+
+    
+
+
+    public static void part1() throws IOException {
+        Scanner in = new Scanner( new File("sampleInput.txt"));
+    }
+
+    public static void part2() throws IOException {
+
+    
+    }
+
+    public static void main(String...args){
+        try{
+            part1();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+    }
+
+
+
+}
